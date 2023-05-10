38ce337c8d5842ee288b3b13599830e32bfd86b2
lotus-systems <spencer.sullivan@gmail.com>
2023-05-09 20:55:50 -0700

    added ID tracker

---
+++ include/ID_Tracker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lotus-systems on 5/9/23.
+//
+
+#include "ID_Tracker.h"
---
+++ include/ID_Tracker.h
@@ -0,0 +1,14 @@
+//
+// Created by lotus-systems on 5/9/23.
+//
+
+#ifndef INC_380_PROJECT_ID_TRACKER_H
+#define INC_380_PROJECT_ID_TRACKER_H
+
+
+class ID_Tracker {
+
+};
+
+
+#endif //INC_380_PROJECT_ID_TRACKER_H
