d85998976c7e7435f1f68b9ed3ed435a51e21135
Spencer Sullivan <spencer.sullivan@gmail.com>
2023-05-12 20:25:23 -0700

    issues

--- include/Issue.h
+++ include/Issue.h
@@ -23,18 +23,25 @@ protected:
     string description;
     string expected_completion_date;
     string actual_completion_date;
-    vector<int> associated_tasks;
+    int associated_task;
+    int associated_action_items[MAX_ARRAY_SIZE] = {0};
+    int associated_decisions[MAX_ARRAY_SIZE] = {0};
 
 public:
-    Issue(int unique_id, string name, string description, string expected_completion_date, string actual_completion_date, vector<int> associated_tasks);
+    Issue(int unique_id, string name, string description, string expected_completion_date, string actual_completion_date, int associated_task, int* associated_action_items, int* associated_decisions);
+    Issue(int unique_id, string name, string description);
     string get_description();
     string get_expected_completion_date();
     string get_actual_completion_date();
-    vector<int> get_associated_tasks();
-    void set_description(string description);
-    void set_expected_completion_date(string expected_completion_date);
-    void set_actual_completion_date(string actual_completion_date);
-    void set_associated_tasks(vector<int> associated_tasks);
+    int get_associated_task();
+    int* get_associated_action_items();
+    int* get_associated_decisions();
+    void set_description(string _description);
+    void set_expected_completion_date(string _expected_completion_date);
+    void set_actual_completion_date(string _actual_completion_date);
+    void set_associated_tasks(int _associated_task);
+    void set_associated_action_items(int* _associated_action_items);
+    void set_associated_decisions(int* _associated_decisions);
     void print();
 
 };
