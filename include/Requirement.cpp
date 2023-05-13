8c690e241e2f45d13a2b8adff5d8ed7867e2605e
Spencer Sullivan <spencer.sullivan@gmail.com>
2023-05-12 20:26:43 -0700

    Requirements

---
+++ include/Requirement.cpp
@@ -0,0 +1,53 @@
+//
+// Created by lotus-systems on 5/11/23.
+//
+
+#include "Requirement.h"
+
+Requirement::Requirement(string requirement_text, string source_doc, string location_in_doc, int associated_deliverable) : base_class(unique_id, name) {
+    this->requirement_text = requirement_text;
+    this->source_doc = source_doc;
+    this->location_in_doc = location_in_doc;
+    this->associated_deliverable = associated_deliverable;
+}
+
+string Requirement::get_requirement_text() {
+    return this->requirement_text;
+}
+
+string Requirement::get_source_doc() {
+    return this->source_doc;
+}
+
+string Requirement::get_location_in_doc() {
+    return this->location_in_doc;
+}
+
+int Requirement::get_associated_deliverable() {
+    return this->associated_deliverable;
+}
+
+void Requirement::set_requirement_text(string _requirement_text) {
+    this->requirement_text = _requirement_text;
+}
+
+void Requirement::set_source_doc(string _source_doc) {
+    this->source_doc = _source_doc;
+}
+
+void Requirement::set_location_in_doc(string _location_in_doc) {
+    this->location_in_doc = _location_in_doc;
+}
+
+void Requirement::set_associated_deliverable(int _associated_deliverable) {
+    this->associated_deliverable = _associated_deliverable;
+}
+
+void Requirement::print() {
+    cout << "Unique ID: " << this->unique_id << endl;
+    cout << "Name: " << this->name << endl;
+    cout << "Requirement Text: " << this->requirement_text << endl;
+    cout << "Source Document: " << this->source_doc << endl;
+    cout << "Location in Document: " << this->location_in_doc << endl;
+    cout << "Associated Deliverable: " << this->associated_deliverable << endl;
+}
---
+++ include/Requirement.h
@@ -0,0 +1,31 @@
+//
+// Created by lotus-systems on 5/11/23.
+//
+
+#ifndef INC_380_PROJECT_REQUIREMENT_H
+#include "base_class.h"
+#define INC_380_PROJECT_REQUIREMENT_H
+
+
+class Requirement: public base_class{
+protected:
+    string requirement_text;
+    string source_doc;
+    string location_in_doc;
+    int associated_deliverable;
+
+public:
+    Requirement(string requirement_text, string source_doc, string location_in_doc, int associated_deliverable);
+    string get_requirement_text();
+    string get_source_doc();
+    string get_location_in_doc();
+    int get_associated_deliverable();
+    void set_requirement_text(string _requirement_text);
+    void set_source_doc(string _source_doc);
+    void set_location_in_doc(string _location_in_doc);
+    void set_associated_deliverable(int _associated_deliverable);
+    void print();
+};
+
+
+#endif //INC_380_PROJECT_REQUIREMENT_H
