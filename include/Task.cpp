207358913958eb5374279dc75f3608a15f0626fe
Spencer Sullivan <spencer.sullivan@gmail.com>
2023-05-12 20:34:01 -0700

    Tasks

--- include/Task.cpp
+++ include/Task.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 Task::Task(int unique_id, string name, string description, string start_date, string end_date, int expected_effort,
-           bool milestone, int dependency, int percent_completion, vector<int> associated_issue, vector<int> associated_resources) : base_class(unique_id, name){
+           bool milestone, int dependency, int percent_completion, int* associated_issue, int associated_resource, int associated_deliverable) : base_class(unique_id, name) {
     this->description = description;
     this->start_date = start_date;
     this->end_date = end_date;
@@ -15,9 +15,26 @@ Task::Task(int unique_id, string name, string description, string start_date, st
     this->milestone = milestone;
     this->dependency = dependency;
     this->percent_completion = percent_completion;
-    this->associated_issue = associated_issue;
-    this->associated_resources = associated_resources;
 
+    memset(this->associated_issues, 0, sizeof(this->associated_issues));
+
+    for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+        this->associated_issues[i] = associated_issue[i];
+    this->associated_resource = associated_resource;
+    this->associated_deliverable = associated_deliverable;
+}
+
+Task::Task(int unique_id, string name) : base_class(unique_id, name) {
+    this->description = "";
+    this->start_date = "";
+    this->end_date = "";
+    this->expected_effort = 0;
+    this->milestone = false;
+    this->dependency = 0;
+    this->percent_completion = 0;
+    memset(this->associated_issues, 0, sizeof(this->associated_issues));
+    this->associated_resource = 0;
+    this->associated_deliverable = 0;
 }
 
 string Task::get_description() {
@@ -48,48 +65,57 @@ int Task::get_percent_completion() {
     return this->percent_completion;
 }
 
-vector<int> Task::get_associated_issue() {
-    return this->associated_issue;
+int* Task::get_associated_issues() {
+    return this->associated_issues;
 }
 
-vector<int> Task::get_associated_resources() {
-    return this->associated_resources;
+int Task::get_associated_resource() {
+    return this->associated_resource;
 }
 
-void Task::set_description(string description) {
-    this->description = description;
+int Task::get_associated_deliverable() {
+    return this->associated_deliverable;
 }
 
-void Task::set_start_date(string start_date) {
-    this->start_date = start_date;
+void Task::set_description(string _description) {
+    this->description = _description;
 }
 
-void Task::set_end_date(string end_date) {
-    this->end_date = end_date;
+void Task::set_start_date(string _start_date) {
+    this->start_date = _start_date;
 }
 
-void Task::set_expected_effort(int expected_effort) {
-    this->expected_effort = expected_effort;
+void Task::set_end_date(string _end_date) {
+    this->end_date = _end_date;
 }
 
-void Task::set_milestone(bool milestone) {
-    this->milestone = milestone;
+void Task::set_expected_effort(int _expected_effort) {
+    this->expected_effort = _expected_effort;
 }
 
-void Task::set_dependency(int dependency) {
-    this->dependency = dependency;
+void Task::set_milestone(bool _milestone) {
+    this->milestone = _milestone;
 }
 
-void Task::set_percent_completion(int percent_completion) {
-    this->percent_completion = percent_completion;
+void Task::set_dependency(int _dependency) {
+    this->dependency = _dependency;
 }
 
-void Task::set_associated_issue(vector<int> associated_issue) {
-    this->associated_issue = associated_issue;
+void Task::set_percent_completion(int _percent_completion) {
+    this->percent_completion = _percent_completion;
 }
 
-void Task::set_associated_resources(vector<int> associated_resources) {
-    this->associated_resources = associated_resources;
+void Task::set_associated_issue(int* _associated_issues) {
+    for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+        this->associated_issues[i] = _associated_issues[i];
+}
+
+void Task::set_associated_resources(int _associated_resource) {
+    this->associated_resource = _associated_resource;
+}
+
+void Task::set_associated_deliverable(int _associated_deliverable) {
+    this->associated_deliverable = _associated_deliverable;
 }
 
 void Task::print() {
@@ -102,7 +128,11 @@ void Task::print() {
     cout << "Milestone: " << this->milestone << endl;
     cout << "Dependency: " << this->dependency << endl;
     cout << "Percent Completion: " << this->percent_completion << endl;
-    //cout << "Associated Issue: " << this->associated_issue << endl;
-    //cout << "Associated Resources: " << this->associated_resources << endl;
+    cout << "Associated Issue: ";
+    for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+        cout << this->associated_issues[i] << " ";
+    cout << endl;
+    cout << "Associated Resource: " << this->associated_resource << endl;
+    cout << "Associated Deliverable: " << this->associated_deliverable << endl;
 }
 
