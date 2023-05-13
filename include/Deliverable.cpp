3b1da7b0931542f10410d251cf72a76132a6b83e
Spencer Sullivan <spencer.sullivan@gmail.com>
2023-05-12 20:21:59 -0700

    deliverables

--- include/Deliverable.cpp
+++ include/Deliverable.cpp
@@ -4,12 +4,24 @@
 
 #include "Deliverable.h"
 
-Deliverable::Deliverable(int unique_id, string name, string description, string expected_completion_date, string actual_completion_date, int associated_tasks, int associated_requirements) : base_class(unique_id, name) {
+Deliverable::Deliverable(int unique_id, string name, string description, string expected_completion_date, string actual_completion_date, int* associated_tasks, int* associated_requirements) : base_class(unique_id, name) {
     this->description = description;
     this->expected_completion_date = expected_completion_date;
     this->actual_completion_date = actual_completion_date;
-    this->associated_tasks = associated_tasks;
-    this->associated_requirements = associated_requirements;
+
+    memset(this->associated_tasks, -1, sizeof(int)*MAX_ARRAY_SIZE);
+    memset(this->associated_requirements, -1, sizeof(int)*MAX_ARRAY_SIZE);
+
+    for(int i = 0; i < sizeof(MAX_ARRAY_SIZE); i++) {
+        this->associated_tasks[i] = associated_tasks[i];
+        this->associated_requirements[i] = associated_requirements[i];
+    }
+}
+
+Deliverable::Deliverable(int unique_id, string name, string description) : base_class(unique_id, name) {
+    memset(this->associated_tasks, -1, sizeof(int)*MAX_ARRAY_SIZE);
+    memset(this->associated_requirements, -1, sizeof(int)*MAX_ARRAY_SIZE);
+    this->description = description;
 }
 
 string Deliverable::get_description() {
@@ -24,11 +36,11 @@ string Deliverable::get_actual_completion_date() {
     return this->actual_completion_date;
 }
 
-int Deliverable::get_associated_tasks() {
+int* Deliverable::get_associated_tasks() {
     return this->associated_tasks;
 }
 
-int Deliverable::get_associated_requirements() {
+int* Deliverable::get_associated_requirements() {
     return this->associated_requirements;
 }
 
@@ -44,12 +56,16 @@ void Deliverable::set_actual_completion_date(string actual_completion_date) {
     this->actual_completion_date = actual_completion_date;
 }
 
-void Deliverable::set_associated_tasks(int associated_tasks) {
-    this->associated_tasks = associated_tasks;
+void Deliverable::set_associated_tasks(int* associated_tasks) {
+    for(int i = 0; i < sizeof(MAX_ARRAY_SIZE); i++) {
+        this->associated_tasks[i] = associated_tasks[i];
+    }
 }
 
-void Deliverable::set_associated_requirements(int associated_requirements) {
-    this->associated_requirements = associated_requirements;
+void Deliverable::set_associated_requirements(int* associated_requirements) {
+    for(int i = 0; i < sizeof(MAX_ARRAY_SIZE); i++) {
+        this->associated_requirements[i] = associated_requirements[i];
+    }
 }
 
 void Deliverable::print() {
